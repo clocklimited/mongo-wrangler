0b3b99f729e96b3ae75d28220477d280c68256cf
Ash Summers <ash.summers@clock.co.uk>
2023-02-03 23:31:26 +0000

    fix(nf): it's always linux

--- dump-nf.sh
+++ dump-nf.sh
@@ -1,15 +1,6 @@
 #!/usr/bin/env bash
 
-ARCH=''
-
-if [[ "$OSTYPE" == "linux-gnu"* ]]; then
-  ARCH='linux'
-elif [[ "$OSTYPE" == "darwin"* ]]; then
-  ARCH='macos'
-else
-  echo "Unsupported operating system, please clone repo and dump." 1>&2
-  exit 1
-fi
+ARCH='linux'
 
 ADDON_ID=$(curl --header "Content-Type: application/json" \
   --header "Authorization: Bearer $NF_API_TOKEN" \
