6f10408438396c935abbab381c969e9ae59149a2
Ash Summers <ash.summers@clock.co.uk>
2023-02-07 20:34:23 +0000

    feat(nf): wait for addon to be running before using it

--- dump-nf.sh
+++ dump-nf.sh
@@ -14,7 +14,19 @@ export OUTPUT=$(curl --header "Content-Type: application/json" \
 
 echo $ADDON_ID
 echo $OUTPUT
-
 echo $NF_API_TOKEN
+
+STATUS=preDeployment
+
+while [[ $STATUS != 'running' ]]
+do
+  STATUS=$(
+    curl --silent -G "https://api.northflank.com/v1/projects/northampton-saints/addons/$ADDON_ID" \
+    -H "Content-Type: application/json" \
+    -H "Authorization: Bearer $NF_API_TOKEN" | jq -r '.data.status'
+  )
+  echo $STATUS
+  sleep 5
+done
 
 "./dist/dump-nf-$ARCH" "$@"
