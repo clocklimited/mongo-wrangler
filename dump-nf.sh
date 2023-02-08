694c8ffc94384c525ce049f5742de953759d6e17
Ash Summers <ash.summers@clock.co.uk>
2023-02-08 22:22:37 +0000

    feat(nf): addon DB name includes timestamp

--- dump-nf.sh
+++ dump-nf.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 
+DATE=$(date +%Y%m%d-%H%M%S)
 export ADDON_ID=$(curl --header "Content-Type: application/json" \
   --header "Authorization: Bearer $NF_API_TOKEN" \
   --request POST \
-  --data '{"name":"mongo-wrangler-staging","description":"Ad-hoc mongo-wrangler db","type":"mongodb","version":"4.2.21","billing":{"deploymentPlan":"nf-compute-100-4","storageClass":"ssd","storage":4096,"replicas":1}}' \
+  --data '{"name":"mongo-wrangler-$DATE","description":"Ad-hoc mongo-wrangler db","type":"mongodb","version":"4.2.21","billing":{"deploymentPlan":"nf-compute-100-4","storageClass":"ssd","storage":4096,"replicas":1}}' \
   https://api.northflank.com/v1/projects/northampton-saints/addons | jq -r '.data.id')
 
 export OUTPUT=$(curl --header "Content-Type: application/json" \
