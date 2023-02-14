9575a8f1cc3a32bdf523de1cab4644431604e442
Ryan Murthick <ryan.murthick@gmail.com>
2023-02-14 17:33:32 +0000

    bug: Add project slug

--- dump-nf.sh
+++ dump-nf.sh
@@ -5,11 +5,11 @@ export ADDON_ID=$(curl --header "Content-Type: application/json" \
   --header "Authorization: Bearer $NF_API_TOKEN" \
   --request POST \
   --data '{"name":"mongo-wrangler-'"$DATE"'","description":"Ad-hoc mongo-wrangler db","type":"mongodb","version":"4.2.21","billing":{"deploymentPlan":"nf-compute-100-4","storageClass":"ssd","storage":4096,"replicas":1}}' \
-  https://api.northflank.com/v1/projects/northampton-saints/addons | jq -r '.data.id')
+  https://api.northflank.com/v1/projects/$SLUG/addons | jq -r '.data.id')
 
 export OUTPUT=$(curl --header "Content-Type: application/json" \
   --header "Authorization: Bearer $NF_API_TOKEN" \
-  https://api.northflank.com/v1/projects/northampton-saints/addons/$ADDON_ID/credentials | jq -r '.data.envs.MONGO_SRV_ADMIN' | sed s#/admin#/#)
+  https://api.northflank.com/v1/projects/$SLUG/addons/$ADDON_ID/credentials | jq -r '.data.envs.MONGO_SRV_ADMIN' | sed s#/admin#/#)
 
 echo $ADDON_ID
 echo $OUTPUT
@@ -20,7 +20,7 @@ STATUS=preDeployment
 while [[ $STATUS != 'running' ]]
 do
   STATUS=$(
-    curl --silent -G "https://api.northflank.com/v1/projects/northampton-saints/addons/$ADDON_ID" \
+    curl --silent -G "https://api.northflank.com/v1/projects/$SLUG/addons/$ADDON_ID" \
     -H "Content-Type: application/json" \
     -H "Authorization: Bearer $NF_API_TOKEN" | jq -r '.data.status'
   )
@@ -33,4 +33,4 @@ node ./dump-nf.js
 curl --header "Content-Type: application/json" \
   --header "Authorization: Bearer $NF_API_TOKEN" \
   --request DELETE \
-  https://api.northflank.com/v1/projects/northampton-saints/addons/${ADDON_ID}
+  https://api.northflank.com/v1/projects/$SLUG/addons/${ADDON_ID}
