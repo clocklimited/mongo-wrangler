877d21f85ef64d9c9a9e29f1055d04f95c0d9c9a
Jack Burgess <jackburgess124@gmail.com>
2023-06-19 18:05:42 +0100

    lint: double quote to prevent globbing/word splitting

--- dump-nf.sh
+++ dump-nf.sh
@@ -14,22 +14,21 @@ export ADDON_ID=$(curl --header "Content-Type: application/json" \
   --header "Authorization: Bearer $NF_API_TOKEN" \
   --request POST \
   --data '{"name":"mongo-wrangler-'"$DATE"'","description":"Ad-hoc mongo-wrangler db","type":"mongodb","version":"4.2.21","billing":{"deploymentPlan":"nf-compute-100-4","storageClass":"ssd","storage":4096,"replicas":1}}' \
-  https://api.northflank.com/v1/projects/$PROJECT_NAME/addons | jq -r '.data.id')
+  "https://api.northflank.com/v1/projects/$PROJECT_NAME/addons" | jq -r '.data.id')
 
 export OUTPUT=$(curl --header "Content-Type: application/json" \
   --header "Authorization: Bearer $NF_API_TOKEN" \
-  https://api.northflank.com/v1/projects/$PROJECT_NAME/addons/$ADDON_ID/credentials | jq -r '.data.envs.MONGO_SRV_ADMIN' | sed s#/admin#/#)
+  "https://api.northflank.com/v1/projects/$PROJECT_NAME/addons/$ADDON_ID/credentials" | jq -r '.data.envs.MONGO_SRV_ADMIN' | sed s#/admin#/#)
 
 STATUS=preDeployment
 
-while [[ $STATUS != 'running' ]]
-do
+while [[ $STATUS != 'running' ]]; do
   STATUS=$(
     curl --silent -G "https://api.northflank.com/v1/projects/$PROJECT_NAME/addons/$ADDON_ID" \
-    -H "Content-Type: application/json" \
-    -H "Authorization: Bearer $NF_API_TOKEN" | jq -r '.data.status'
+      -H "Content-Type: application/json" \
+      -H "Authorization: Bearer $NF_API_TOKEN" | jq -r '.data.status'
   )
-  echo $STATUS
+  echo "$STATUS"
   sleep 5
 done
 
@@ -38,4 +37,4 @@ node ./dump-nf.js
 curl --header "Content-Type: application/json" \
   --header "Authorization: Bearer $NF_API_TOKEN" \
   --request DELETE \
-  https://api.northflank.com/v1/projects/$PROJECT_NAME/addons/${ADDON_ID}
+  "https://api.northflank.com/v1/projects/$PROJECT_NAME/addons/${ADDON_ID}"
